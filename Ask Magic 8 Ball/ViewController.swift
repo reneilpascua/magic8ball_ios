7b292d8432dcdbb0156cff9f2b6a67ed877a737e
reneilpascua <rpascua2@my.bcit.ca>
2019-12-23 17:28:47 -0800

    Initial Commit

---
+++ Ask Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ask Magic 8 Ball
+//
+//  Created by Reneil Pascua on 2019-12-23.
+//  Copyright © 2019 Reneil Pascua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
